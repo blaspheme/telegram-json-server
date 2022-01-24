7e13932c096dac39599d149f3cddedf7b0622231
erudite <mooncrime93@gmail.com>
2022-01-24 18:47:23 +0800

    增加路由重写

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+  '/tg/*': '/$1',
+}))
+
 server.use(router)
+
 server.listen(8080, () => {
   console.log('JSON Server is running')
 })
